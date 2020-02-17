d2b3412b5eccd0a95a66da3cc98e21a216c26a5e
Jackson <jacksonsr45@gmail.com>
2020-02-17 00:10:08 -0300

    add components button, frame, label, entry

--- app/src/data/app_login.py
+++ app/src/data/app_login.py
@@ -11,11 +11,30 @@ class Login:
         x = (self.root.winfo_screenwidth() // 2) - (400 // 2)
         y = (self.root.winfo_screenheight() // 2) - (200 // 2)
         self.root.geometry("400x200+{}+{}".format( x, y))
+        self.root.resizable(width=False, height=False)
 
         self.msg = tkinter.StringVar()
         self.username = tkinter.StringVar()
         self.password = tkinter.StringVar()
 
+        self.frame = self.__frame(self.root, "white", "black", 1, 0.05, 0.05, 0.9, 0.9)
+
+        self.__label(self.frame, ("Ubunto", 14), "white", "Name:", 0, 0.2, 0.3, 0.15)
+
+        self. name = self.__entry(self.frame, self.username, ("Ubunto", 14), "normal", 
+                                "white", 0.3, 0.2, 0.6, 0.15)
+        self.name.focus()
+
+        self.__label(self.frame, ("Ubunto", 14), "white", "Password:", 0, 0.5, 0.3, 0.15)
+
+        self.__entry(self.frame, ("Ubunto", 14), self.password, "normal", "white", 
+                        0.3, 0.5, 0.6, 0.15)
+
+        self.__button(self.root, "white", ("Ubunto", 14), "CREATE USER", "normal", 0, 
+                        0.1, 0.7, 0.35, 0.15)
+        
+        self.__button(self.root, "white", ("Ubunto", 14), "LOGIN", "normal", 0, 
+                        0.5, 0.7, 0.35, 0.15)
 
 
     def set_msg(self, msg):
@@ -29,7 +48,7 @@ class Login:
             messagebox.showinfo('msg', self.msg.get())
 
 
-    def __labels(self, root, font, bg, text, x, y, width, height):
+    def __label(self, root, font, bg, text, x, y, width, height):
         label = tkinter.Label(root)
         label.configure(font=font)
         label.configure(anchor=tkinter.E)
@@ -39,17 +58,18 @@ class Login:
         return label
 
 
-    def __entry(self, root, font, bg, x, y, width, height):
+    def __entry(self, root, font, textvariable, state, bg, x, y, width, height):
         entry = tkinter.Entry(root)
         entry.configure(font=font)
+        entry.configure(textvariable=textvariable)
         entry.configure(bd=0)
-        entry.configure(state=tkinter.DISABLED)
+        entry.configure(state=state)
         entry.configure(bg=bg)
         entry.place(relx=x, rely=y, relwidth=width, relheight=height)
         return entry
 
 
-    def __buttons(self, root, bg, font, text, state, bd, x, y, width, height, command=None):
+    def __button(self, root, bg, font, text, state, bd, x, y, width, height, command=None):
         button = tkinter.Button(root)
         button.configure( bg=bg )
         button.configure( font=font )
@@ -61,7 +81,7 @@ class Login:
         return button
     
 
-    def __frames(self, root, bg, hlbg, hlbd, x, y, width, height):
+    def __frame(self, root, bg, hlbg, hlbd, x, y, width, height):
         frame = tkinter.Frame(root)
         frame.configure(bg=bg)
         frame.configure( highlightbackground=hlbg )
